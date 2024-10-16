b0e631f6fd002b7e6df5fcff148f879444314482
jithin8223 <jithinpbabu7020@gmail.com>
2024-10-16 15:19:28 +0530

    Api_Login

---
+++ testmod/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from testmodapp.views  import *
+
+
+urlpatterns=[
+    path('register/',Registerview.as_view()),
+    path('login/',Loginview.as_view()),
+    path('user/',UserView.as_view()),
+    path('logout/',LogoutView.as_view())
+]
---
+++ testmod/testmodapp/views.py
@@ -0,0 +1,70 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from.serializers import *
+from.models import *
+from rest_framework.exceptions import AuthenticationFailed
+import jwt,datetime
+
+
+
+class Registerview(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class Loginview(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        
+        if not user.check_password(password):
+            raise AuthenticationFailed('Invalid credentials')
+        
+        payload = {
+            'id': user.id,
+            'email': user.email,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow(),
+
+        }
+        token = jwt.encode(payload, 'secret_key', algorithm='HS256')
+
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True) 
+        response.data = {
+            'jwt': token
+        }
+        return response
+
+       
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Authentication token is missing')
+        try:
+            payload = jwt.decode(token,'secret_key', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Token expired')
+        
+        user= User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data ={
+            'message':'success'
+        }
+        return response 
